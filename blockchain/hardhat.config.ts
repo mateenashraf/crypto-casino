56e1f01f35981d40101e0724c75af95471d27d0b
mateenashraf <mateen.ash@gmail.com>
2026-07-05 04:48:14 -0400

    Add blockchain/hardhat.config.ts

---
+++ blockchain/hardhat.config.ts
@@ -0,0 +1,32 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
+
+const SEPOLIA_RPC = process.env.SEPOLIA_RPC_URL || "";
+const DEPLOYER_KEY = process.env.DEPLOYER_PRIVATE_KEY || "";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: { enabled: true, runs: 200 },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: SEPOLIA_RPC || "https://rpc.sepolia.org",
+      accounts: DEPLOYER_KEY ? [DEPLOYER_KEY] : [],
+    },
+  },
+};
+
+export default config;
