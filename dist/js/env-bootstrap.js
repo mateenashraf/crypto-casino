b62f6e1522d38cbdf0d6685d126ade9102694747
mateenashraf <mateen.ash@gmail.com>
2026-07-09 20:50:17 +0100

    Update dist/js/env-bootstrap.js

--- dist/js/env-bootstrap.js
+++ dist/js/env-bootstrap.js
@@ -1,1 +1,1 @@
-!function(){const e=location.hostname,n="localhost"===e||"127.0.0.1"===e;if(window.__ND_CFG__=Object.freeze({apiBase:"",treasury:""}),n){const e=document.createElement("script");e.src="js/dev-grants.js?v=neondraw",e.defer=!0,document.head.appendChild(e)}}();
+!function(){const e=location.hostname;if("localhost"===e||"127.0.0.1"===e){const e=document.createElement("script");e.src="js/dev-grants.js?v=neondraw",e.defer=!0,document.head.appendChild(e)}}();
